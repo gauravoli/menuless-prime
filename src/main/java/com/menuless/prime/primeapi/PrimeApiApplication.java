d03af650910cc4f35adac882ef91a392b5b5ddd3
gaurav oli <gaurav.oli@capco.com>
2019-05-12 10:25:11 -0400

    initial commit - project setup

---
+++ src/main/java/com/menuless/prime/primeapi/PrimeApiApplication.java
@@ -0,0 +1,13 @@
+package com.menuless.prime.primeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/menuless/prime/primeapi/PrimeApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.menuless.prime.primeapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrimeApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
